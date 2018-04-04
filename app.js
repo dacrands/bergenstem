10479b83595dd037fd39532aefe40daf85a12ac0
David Crandall <davecrands@gmail.com>
2018-04-04 16:58:12 -0400

    Added about page

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
-const express    = require('express'),
-      app        = express(),
-      ejs        = require('ejs'),
-      mongoose   = require('mongoose'),
-      seedDB     = require('./seed'),
-      bodyParser = require('body-parser');
+const express       = require('express'),
+      app           = express(),
+      ejs           = require('ejs'),
+      mongoose      = require('mongoose'),
+      seedDB        = require('./seed'),
+      bodyParser    = require('body-parser'),
+      passport      = require('passport'),
+      localStrategy = require('passport-local'),
+      Admin         = require('./models/admin');
 
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/src'));
+
+// PASSPORT
+app.use(require('express-session')({
+    secret: "Time is fun",
+    resave: false,
+    saveUnitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(Admin.authenticate()));
+passport.serializeUser(Admin.serializeUser());
+passport.deserializeUser(Admin.deserializeUser());
       
-//MongoDB
+// MONGODB
 mongoose.connect(process.env.DATABASEURL);
-seedDB();
+// seedDB();
 
 // ROUTES
 const indexRoutes   = require('./routes/index');
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
 const express = require('express'),
       router  = express.Router();
 
-router.get("/", (req, res) =>{
+router.get('/', (req, res) =>{
     res.render("index");
 });
 
-router.get("/contact", (req, res) => {
-    res.render("contact");
-})
+router.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+router.get('/about', (req, res) => {
+    res.render('about');
+});
 
 module.exports = router;
